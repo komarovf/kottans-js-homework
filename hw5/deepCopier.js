f6104cf7909bc7d595924761330e7c2370d6ca84
Serhii <komarovf88@gmail.com>
2014-11-11 15:00:18 +0000

    hw5(js)

--- hw5/deepCopier.js
+++ hw5/deepCopier.js
@@ -1,9 +1,16 @@
-// Your code goes here
 function deepCopy(obj) {
+  if ((typeof obj === 'undefined') || (obj === null) || isFinite(String(obj))) {
+    return obj;
+  }
   result = {};
   for (var i in obj) {
     if (obj.hasOwnProperty(i)) {
-      result.i = obj.i;
+      if (obj[''+i] instanceof Array) {
+        result[''+i] = obj[''+i].slice();
+      }
+      else {
+        result[''+i] = obj[''+i];
+      }
     }
   }
   return result;
